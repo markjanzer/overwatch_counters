27c8a32716b46ac96255f33a2679bffd78d6e962
markjanzer <mark.janzer@gmail.com>
2016-06-25 16:29:04 -0700

    Add foundation gem.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,11 @@
 //= require jquery
 //= require jquery_ujs
+//= require foundation
 //= require turbolinks
 //= require react
 //= require react_ujs
 //= require isotope.pkgd.min.js
 //= require_tree .
 
+
+$(function(){ $(document).foundation(); });
