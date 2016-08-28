f144ff719d9b9fe321b0c63a7ba98e7544c30707
markjanzer <mark.janzer@gmail.com>
2016-08-27 18:15:10 -0700

    Create OverwatchCounters component

---
+++ app/assets/javascripts/components/OverwatchCounters.jsx
@@ -0,0 +1,12 @@
+class OverwatchCounters extends React.Component {
+	constructor(props, context) {
+		super(props, context);
+		this.heroes = ["genji", "mccree", "pharah", "reaper", "soldier-76", "tracer", "bastion", "hanzo", "junkrat", "mei", "torbjorn", "widowmaker", "dva", "reinhardt", "roadhog", "winston", "zarya", "ana", "lucio", "mercy", "symmetra", "zenyatta" ];
+	}
+
+	render() {
+		return (
+			
+		);	
+	}
+}
